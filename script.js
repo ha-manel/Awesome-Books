3df551d069e7b9083b49adcc0129f5876d0cc68b
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-04-19 14:34:39 +0200

    Add styles

--- script.js
+++ script.js
@@ -49,3 +49,34 @@ addBook.addEventListener('click', (e) => {
   titleInput.value = '';
   authorInput.value = '';
 });
+
+const booksCollection = new BookCollection();
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addBook = document.querySelector('#add-book');
+ 
+const booksDiv = document.querySelector('.books');
+function displayBooks() {
+  booksDiv.innerHTML = '';
+  booksCollection.booksArray.forEach((book, id) => {
+    booksDiv.innerHTML += `<div class="book-container"><div class="book-info"><p class="display-title">" ${book.title.charAt(0).toUpperCase() + book.title.slice(1)} "</p><p>by</p><p class="display-author">${book.author.charAt(0).toUpperCase() + book.author.slice(1)}</p></div><button id="${id}" class="remove-btn" onclick="remove(this.id)">Remove</button></div>`;
+  });
+}
+ 
+displayBooks();
+ 
+function remove(bookId) { //eslint-disable-line
+  booksCollection.removeBook(bookId);
+  displayBooks();
+}
+ 
+addBook.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (titleInput.value && authorInput.value) {
+    booksCollection.addBook(titleInput.value, authorInput.value);
+    displayBooks();
+ 
+    titleInput.value = '';
+    authorInput.value = '';
+  }
+});
